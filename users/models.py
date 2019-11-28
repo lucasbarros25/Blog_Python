d49e36209dc3b8b4878f7523f5ac66fb8e109594
lucasoutor <lucasoutor@gmail.com>
2019-11-28 01:58:51 -0300

    Bug fix

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
@@ -10,8 +11,8 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
